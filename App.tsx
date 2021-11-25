92013e86c653747d9e11e83c48ed8d42cee3edb1
Ishrak Imam <ishrak@litentry.com>
2021-11-25 21:51:39 +0100

    Network provider

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from 'navigation/AppNavigator';
 import {ThemeProvider} from 'context/ThemeContext';
+import { NetworkProvider } from 'context/Network';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <ThemeProvider>
-          <AppNavigator />
-        </ThemeProvider>
-      </NavigationContainer>
+      <NetworkProvider>
+        <NavigationContainer>
+          <ThemeProvider>
+            <AppNavigator />
+          </ThemeProvider>
+        </NavigationContainer>
+      </NetworkProvider>
     </SafeAreaProvider>
   );
 }
---
+++ src/context/Network.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import {noop} from 'lodash';
+import {usePersistedState} from 'hooks/usePersistedState';
+
+type SupportedNetwork = 'polkadot' | 'kusama'
+
+type Network = {
+  name: string;
+  key: SupportedNetwork;
+  ws: string[];
+  isTestnet?: boolean;
+  color?: string;
+  ss58Format: number;
+};
+
+export type NetworkContext = {
+  currentNetwork: Network;
+  availableNetworks: Network[];
+  select: (network: Network) => void;
+};
+
+const PolkadotNetwork: Network = {
+  name: 'Polkadot',
+  key: 'polkadot',
+  ws: ['wss://rpc.polkadot.io'],
+  color: '#800000',
+  ss58Format: 0,
+};
+
+const KusamaNetwork: Network = {
+  name: 'Kusama',
+  key: 'kusama',
+  ws: ['wss://kusama.api.onfinality.io/public-ws'],
+  color: '#e6194B',
+  ss58Format: 2,
+};
+
+
+const availableNetworks = [
+  PolkadotNetwork,
+  KusamaNetwork,
+];
+
+const NetworkContext = React.createContext<NetworkContext>({
+  currentNetwork: PolkadotNetwork,
+  availableNetworks: availableNetworks,
+  select: noop,
+});
+
+type PropTypes = {
+  children: React.ReactNode;
+};
+
+export function NetworkProvider({children}: PropTypes) {
+  const [currentNetwork, setCurrentNetwork] = usePersistedState<Network>('network', PolkadotNetwork);
+
+  const value = React.useMemo(
+    () => ({currentNetwork, availableNetworks, select: setCurrentNetwork}),
+    [currentNetwork, setCurrentNetwork],
+  );
+
+  return <NetworkContext.Provider value={value}>{children}</NetworkContext.Provider>;
+}
+
+export function useNetwork() {
+  const context = React.useContext(NetworkContext)
+
+  if(!context) {
+    throw new Error('useNetwork must be used within a NetworkProvider');
+  }
+
+  return context
+}
